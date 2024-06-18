26620ce3b2789468c8909602f4f7f66210e95987
Luciano Alive <lucianoalivegit@gmail.com>
2024-06-18 16:15:00 -0300

    arreglos en html

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+//requirimiento de express
 const express = require("express");
 const path = require("node:path");
+//instancia de express
 const app = express();
+//definicion del pueto de escucha
 const PORT = process.env.PORT ?? 3000;
 
+//establecer carptea public como carpeta de achivos estaticos
 app.use(express.static('./public'));
 
+//deficnion de rutas
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("views/home.html"));
 });
 
+//inicio de servidor
 app.listen(PORT,(err)=>{
     err
         ? console.error("Server failed: ", err.message)
